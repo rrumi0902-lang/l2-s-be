5854fa5468bdd5d6df33f379c1a505a5dea80463
Moderator11 <p7925221@gmail.com>
2025-12-06 04:54:38 +0900

    feat: connection pool setting optimized

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config.environments import SUPABASE_DB_URL
+from sqlalchemy.pool import QueuePool
 
-engine = create_engine(SUPABASE_DB_URL, pool_pre_ping=True)
+engine = create_engine(SUPABASE_DB_URL,
+                       pool_pre_ping=True,
+                       poolclass=QueuePool,
+                       pool_size=5,
+                       max_overflow=10,
+                       pool_timeout=30,
+                       pool_recycle=3600)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
