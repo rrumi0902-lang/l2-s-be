83dfed620608a27602245c5572dfd964de9145c0
rumi <rrumi0902@gmail.com>
2025-12-24 11:00:45 +0900

    revised the code to ignore the .env

--- app/middleware/cors.py
+++ app/middleware/cors.py
@@ -1,10 +1,9 @@
 from fastapi.middleware.cors import CORSMiddleware
 
-
 def add_cors(application):
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=ALLOWED_ORIGINS,
+        allow_origins=["*"],  
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
