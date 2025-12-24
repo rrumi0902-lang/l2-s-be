c5d6a1eef72a0423f544f0b1cdfb98a8706f797f
rumi <rrumi0902@gmail.com>
2025-12-24 14:18:21 +0900

    directing the exact address of the FE

--- app/middleware/cors.py
+++ app/middleware/cors.py
@@ -3,7 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 def add_cors(application):
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  
+        allow_origins=[
+            "https://shortcake-fe.vercel.app",
+            "https://shortcake-lqavsdtie-melaka.vercel.app" # 에러에 뜬 미리보기 주소도 추가
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
